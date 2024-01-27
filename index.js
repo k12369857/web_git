4ef67d06d396d9f9df8948c5dff48c4baa98ddc7
Jia_Hao <wujiahao@wujiahaodeMacBook-Air.local>
2024-01-27 23:52:37 +0800

    message

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const menuOpen = document.getElementById('menu-open');
+const sideBar = document.querySelector('.menu_left');
 
-  // 添加点击文档的事件监听器
-  document.addEventListener('click', function(event) {
-    var expandToggle = document.getElementById('expand-toggle');
-    var languageMenu = document.querySelector('.Language_Bar_Menu');
+menuOpen.addEventListener('click', () => {
+    sideBar.style.width = '45%';
+});
 
-    // 检查点击的元素是否在 #expand-toggle 或 .language-menu 区域内
-    if (!expandToggle.contains(event.target) && !languageMenu.contains(event.target)) {
-      // 如果不是，则取消 #expand-toggle 的选中状态
-      expandToggle.checked = false;
+ document.addEventListener('click', function(event) {
+   
+    if (!sideBar.contains(event.target) && !menuOpen.contains(event.target)) {
+  
+      sideBar.style.width = '0%';
     }
   });
